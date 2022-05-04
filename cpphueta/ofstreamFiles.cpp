aa62e5b9aaf6f5c412ace5abd3d6e9c02ac6cc89
Azatbek <72803057+CaptainHazmat@users.noreply.github.com>
2022-05-04 19:05:33 +0600

    Update ofstreamFiles.cpp

--- cpphueta/ofstreamFiles.cpp
+++ cpphueta/ofstreamFiles.cpp
@@ -5,17 +5,31 @@ using namespace std;
 int main()
 {
 	string path;
+	char cans;
+	bool bans = true;
+	cout << "NAME YOUR FILE: ";
 	cin >> path;
+	ofstream port;
+	port.open(path + ".txt", ofstream::app);
 
-	ofstream fort;
-	fort.open(path + ".txt", ofstream::app);	// create file , do not erase old info
-
-	if (!fort.is_open()) {		// whether file opened
-		cout << "ERROR" << endl;
+	if (port.is_open()) {
+		do {
+			cout << "DO YOU WANT TO TYPE SMTH?: Y/N: ";
+			cin >> cans;
+			if (cans == 'Y' || cans == 'y') {
+				cout << "PRINT THE TEXT " << endl;
+				for (int row = 0; row < 10; row++) {
+					string text;
+					cin >> text;
+					port << text << endl;
+				}
+			}
+			else
+				bans = false;
+		} while (bans);
 	}
 	else {
-		fort << "542";		// insert new info to file
+		cout << "ERROR";
 	}
-
-	fort.close();		// clean the memory
+	port.close();
 }
