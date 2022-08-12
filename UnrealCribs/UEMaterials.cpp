91cdd45dd1d775b2292a662bede1a56f3f2e494e
Azatbek <72803057+HazmatG@users.noreply.github.com>
2022-08-12 21:43:25 +0600

    Create UEMaterials.cpp

---
+++ UnrealCribs/UEMaterials.cpp
@@ -0,0 +1,29 @@
+// Materials , change meshes material
+ 
+// .h file
+
+UPROPERTY(EditAnywhere)
+FLinearColor NewColour = FLinearColor::Black;     // default material variable.
+
+
+// .cpp file
+
+#include "Materials/MaterialInstanceDynamic.h"
+
+void ABaseGeometryActor::SetColor(const FLinearColor& Color)
+{
+	UMaterialInstanceDynamic* MyIns = MyMesh->CreateAndSetMaterialInstanceDynamic(0);	// checks materials in mesh, (Materials num)
+	if (MyIns) {
+		MyIns->SetVectorParameterValue("MyColor", Color);	// Sets values on Material (Parameters name, Function parameter)
+	}
+}
+
+// Change Material per second
+
+FTimerHandle TimerHandle;
+
+UPROPERTY(EditAnywhere)
+	float TimerRate = 3.0f;
+
+GetWorldTimerManager().SetTimer(TimerHandle, this, &ABaseGeometryActor::OnTimerFired, TimerRate, true);		
+// TimerHandle, pointer on object to call func, function to change color, frequency variable, looped 
