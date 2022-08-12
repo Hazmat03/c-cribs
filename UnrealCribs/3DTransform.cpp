fc0381102b73e9abdec9135b5d68b1c07271cc29
Azatbek <72803057+HazmatG@users.noreply.github.com>
2022-08-12 21:59:18 +0600

    Update and rename 1.cpp to 3DTransform.cpp

---
+++ UnrealCribs/3DTransform.cpp
@@ -0,0 +1,20 @@
+// LOG
+UE_LOG(LogTemp, Warning, TEXT("Actors Name"));
+
+// Create Mesh
+UStaticMeshComponent* 		// Create StaticMesh
+UShapeComponent*		// Create Shape
+
+// Set 3D
+mesh->SetWorldScale3D(FVector(Xf, Yf, Zf));
+mesh->SetWorldLocation(FVector( X, Y, Z));
+
+// Create Static Mesh
+UPROPERTY(EditAnywhere)
+UStaticMeshComponent* mesh;
+
+mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Meshes Name"));
+
+// Root Component
+SetRootComponent(mesh);
+NewMesh->AttachTo(RootComponent);
