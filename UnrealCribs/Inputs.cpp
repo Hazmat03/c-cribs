c417163f5fba9e9ca726355f1c71f9233e526c9b
Azatbek <72803057+HazmatG@users.noreply.github.com>
2022-08-27 19:21:55 +0600

    Update Inputs.cpp

--- UnrealCribs/Inputs.cpp
+++ UnrealCribs/Inputs.cpp
@@ -1,10 +1,16 @@
 	// Set Mouse Cursor On
-	APlayerController* PlayerController = GetFirstLocalPlayerController();
+APlayerController* PlayerController = GetFirstLocalPlayerController();
 
-	FInputModeGameAndUI InputModeData;
-	InputModeData.SetWidgetToFocus(MainMenu->TakeWidget());
-	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+FInputModeGameAndUI InputModeData;
+InputModeData.SetWidgetToFocus(MainMenu->TakeWidget());
+InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
 
-	PlayerController->SetInputMode(InputModeData);
+PlayerController->SetInputMode(InputModeData);
 
-	PlayerController->bShowMouseCursor = true;
+PlayerController->bShowMouseCursor = true;
+
+
+// Delegates
+// Data types that reference and execute member functions on C++ Objects
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnColorChanged, const FLinearColor&, Color, const FString&, Name);
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnTimerFinished, AActor*);
