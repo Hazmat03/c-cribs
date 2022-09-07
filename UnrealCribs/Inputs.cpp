57c418383b944f03a57c833621d42d40f8f0d324
Azatbek <72803057+HazmatG@users.noreply.github.com>
2022-09-07 22:05:34 +0600

    Update Inputs.cpp

--- UnrealCribs/Inputs.cpp
+++ UnrealCribs/Inputs.cpp
@@ -14,3 +14,28 @@ PlayerController->bShowMouseCursor = true;
 // Data types that reference and execute member functions on C++ Objects
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnColorChanged, const FLinearColor&, Color, const FString&, Name);
 DECLARE_MULTICAST_DELEGATE_OneParam(FOnTimerFinished, AActor*);
+
+
+// Set Inputs
+// Axis Mapping
+
+#include "Components/InputComponent.h"
+
+void AFrickinPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &AFrickinPawn::MoveForward);	
+	// (Mappings name, pointer to class, called function)
+	
+	PlayerInputComponent->BindAxis("MoveRight", this, &AFrickinPawn::MoveRight);
+}
+
+// Action Mapping
+
+void AFuckinPC::SetupInputComponent() {
+	Super::SetupInputComponent();
+
+	InputComponent->BindAction("ChangePawn", IE_Pressed, this, &AFuckinPC::ChangePawn);	
+	// (Mappings name, enum for event type pointer to class, called function)
+}
